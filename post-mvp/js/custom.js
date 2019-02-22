59607745b1cce4b9a581f4939ec6ec28ad3a73d4
carolynw <carolynwarburton@hotmail.com>
2019-02-22 10:30:23 +0000

    Add new prototype

---
+++ post-mvp/js/custom.js
@@ -0,0 +1,47 @@
+/* Sidebar navigation */
+/* ------------------ */
+
+/* Show navigation when the width is greather than or equal to 991px */
+$(document).ready(function() {
+
+ 
+
+$(document).ready(function(){
+  var hash = window.location.hash;
+if (hash == "#previous"){
+  
+}
+
+ 
+});
+
+
+
+  $("#btn-add-new-submission").on("click", function(e){
+    $("#js-file-type").hide();
+    $("#js-add-new-submission").show();
+    //$("#js-form-submit-data").hide();
+    //$("#js-list-submissions").show();
+  })
+
+  $("#btn-add-file").on("click", function(e){
+    $("#js-file-type").hide();
+    $("#js-add-new-submission").hide();
+    $("#js-form-submit-data").show();
+    //$("#js-list-submissions").show();
+  })
+
+  $(window).resize(function()
+  {
+    if($(window).width() >= 767){
+      $(".side-nav").slideDown(150);
+    }     
+	else{
+	  $(".side-nav").slideUp(150);
+	}	
+  });
+
+});
+
+
+ 
